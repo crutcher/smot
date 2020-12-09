c4c80a3a8d1f5a0f7dfd533ca2e696537e801c6e
Crutcher Dunnavant <crutcher@gmail.com>
2020-12-08 17:11:05 -0800

    Add Expect.is_eq(); refactor is_truthy, is_falsey

--- smot/common/expect.py
+++ smot/common/expect.py
@@ -14,19 +14,67 @@ class Expect:
   """
 
   @staticmethod
-  def truthy(
-    test: bool,
-    msg: str = "Value is not truthy.",
+  def is_truthy(
+    actual: Any,
+    msg: str = "Value is not truthy: %(actual)s",
     cls: Type[Exception] = AssertionError,
     **kwargs: Any,
   ) -> None:
     """
-    Expect that a value is truthy.
+    Expect that a value is is_truthy.
 
-    :param test: the value to test.
+    :param actual: the value to test.
     :param msg: the message template.
     :param cls: the exception class.
     :param kwargs: any additional keyword args for the message.
     """
-    if not test:
-      raise cls(msg % dict(kwargs))
+    if not actual:
+      raise cls(msg % dict(
+        actual=actual,
+        **kwargs,
+      ))
+
+  @staticmethod
+  def is_falsey(
+    actual: Any,
+    msg: str = "Value is not falsey: %(actual)s",
+    cls: Type[Exception] = AssertionError,
+    **kwargs: Any,
+  ) -> None:
+    """
+    Expect that a value is falsey.
+
+    :param actual: the value to test.
+    :param msg: the message template.
+    :param cls: the exception class.
+    :param kwargs: any additional keyword args for the message.
+    """
+    if actual:
+      raise cls(msg % dict(
+        actual=actual,
+        **kwargs,
+      ))
+
+  @staticmethod
+  def is_eq(
+    actual: Any,
+    expected: Any,
+    msg: str = "Value (%(actual)s) != (%(expected)s)",
+    cls: Type[Exception] = AssertionError,
+    **kwargs: Any,
+  ) -> None:
+    """
+    Expect that a value is is_truthy.
+
+    :param actual: the value to test.
+    :param expected: the value to compare.
+    :param msg: the message template.
+    :param cls: the exception class.
+    :param kwargs: any additional keyword args for the message.
+    """
+    if actual != expected:
+      raise cls(msg % dict(
+        actual=actual,
+        expected=expected,
+        **kwargs,
+      ))
