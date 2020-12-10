80f7a7050ae4644f113c3c3faab5c2a90c9251d6
Crutcher Dunnavant <crutcher@gmail.com>
2020-12-09 22:22:38 -0800

    Refactor reports

--- smot/jupyter/model_reports.py
+++ smot/jupyter/model_reports.py
@@ -1,16 +1,77 @@
-from typing import Tuple
+from typing import Tuple, Optional
 
 import nptyping
 import tensorflow as tf
+from matplotlib import pyplot as plt
 
-from smot.jupyter import plot_funcs
+
+def _plot_model_fit(
+  history: tf.keras.callbacks.History,
+  test_eval: Optional[Tuple[float, float]] = None,
+) -> None:
+  """
+  Plot training history.
+
+  :param history: the history to plot.
+  :param test_eval: optional, the (test_loss, test_accuracy).
+  """
+  plt.style.use({'figure.facecolor': 'white'})
+
+  fig, ax1 = plt.subplots()
+
+  plt.title('model fit report')
+
+  has_val = "val_loss" in history.history
+
+  ax1.set_ylabel("loss", color="red")
+  ax1.plot(history.history["loss"], color='red')
+
+  if has_val:
+    ax1.plot(history.history["val_loss"], color='red', dashes=[5])
+
+  ax2 = ax1.twinx()
+  ax2.set_xlabel("Epochs")
+  ax2.set_ylabel("accuracy", color="blue")
+
+  legend_handles = []
+  train_handle, = ax2.plot(
+    history.history["accuracy"],
+    color='blue',
+    label='train',
+  )
+  legend_handles.append(train_handle)
+
+  if has_val:
+    val_handle, = ax2.plot(
+      history.history["val_accuracy"],
+      color='blue',
+      dashes=[5],
+      label='val',
+    )
+    legend_handles.append(val_handle)
+
+  if test_eval:
+    test_loss, test_accuracy = test_eval
+    ax1.axhline(test_loss, color='lightcoral', linestyle='dotted')
+
+    test_handle = ax2.axhline(
+      test_accuracy,
+      color='dodgerblue',
+      linestyle='dotted',
+      label='test',
+    )
+    legend_handles.append(test_handle)
+
+  ax2.legend(handles=legend_handles, loc='lower left')
+
+  plt.show()
 
 
 def model_fit_report(
-  *,
-  model: tf.keras.models.Model,
-  history: tf.keras.callbacks.History,
-  test_data: Tuple[nptyping.NDArray, nptyping.NDArray],
+    *,
+    model: tf.keras.models.Model,
+    history: tf.keras.callbacks.History,
+    test_data: Tuple[nptyping.NDArray, nptyping.NDArray],
 ) -> Tuple[float, float]:
   """
   Evaluate a model against its test data, and graph the results.
@@ -35,6 +96,6 @@ def model_fit_report(
 
   print(f"Test loss: {test_loss}, Test accuracy: {test_accuracy}")
 
-  plot_funcs.plot_history(history, (test_loss, test_accuracy))
+  _plot_model_fit(history, (test_loss, test_accuracy))
 
   return (test_loss, test_accuracy)
