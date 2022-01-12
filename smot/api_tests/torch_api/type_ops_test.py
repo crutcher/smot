fcf546072fb94b03f6b4e70d4636845137e2aaf1
Crutcher Dunnavant <crutcher@gmail.com>
2022-01-12 10:21:34 -0800

    refactor shape ops

--- smot/api_tests/torch_api/type_ops_test.py
+++ smot/api_tests/torch_api/type_ops_test.py
@@ -238,30 +238,3 @@ class TypeOpsTest(unittest.TestCase):
             lambda: torch.is_nonzero("abc"),
             TypeError,
         )
-
-    def test_numel(self):
-        """torch.numel(input: Tensor)
-        Also: `<tensor>.numel()`
-
-        Returns the number of elements in the tensor.
-
-        .. _Online Doc:
-            https://pytorch.org/docs/stable/generated/torch.numel.html
-        """
-        for s in [1, [1], [[1]]]:
-            t = torch.tensor(s)
-            eggs.assert_match(torch.numel(t), 1)
-            eggs.assert_match(t.numel(), 1)
-
-        for s in [[1, 2, 3, 4], [[1, 2], [3, 4]], [[1], [2], [3], [4]]]:
-            t = torch.tensor(s)
-            eggs.assert_match(torch.numel(t), 4)
-            eggs.assert_match(t.numel(), 4)
-
-        # Errors:
-        # =======
-        # Throws RuntimeError if type is not floating point.
-        eggs.assert_raises(
-            lambda: torch.numel([1, 2]),
-            TypeError,
-        )
