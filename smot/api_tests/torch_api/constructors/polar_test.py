539caef21504a7811943c3dd94e335855fc44278
Crutcher Dunnavant <crutcher@gmail.com>
2022-01-13 13:28:23 -0800

    polar

--- smot/api_tests/torch_api/constructors/polar_test.py
+++ smot/api_tests/torch_api/constructors/polar_test.py
@@ -10,13 +10,13 @@ class PolarTest(unittest.TestCase):
     # https://pytorch.org/docs/stable/generated/torch.polar.html
 
     def test_polar(self):
-        abs = torch.tensor([1, 2], dtype=torch.float64)
-        angle = torch.tensor([np.pi / 2, 2 * np.pi], dtype=torch.float64)
+        abs = torch.tensor([1, 2, 1], dtype=torch.float64)
+        angle = torch.tensor([np.pi / 2, 2 * np.pi, 5 * np.pi / 2], dtype=torch.float64)
 
         torch_eggs.assert_tensor_close(
             torch.polar(abs, angle),
             torch.tensor(
-                [0.0 + 1.0j, 2.0],
+                [0.0 + 1.0j, 2.0, 0.0 + 1.0j],
                 dtype=torch.complex128,
             ),
         )
