0718c01ae9b08f815d5f2cbfc453f9e93b15b7f0
Crutcher Dunnavant <crutcher@gmail.com>
2022-01-12 18:58:32 -0800

    linspace/logspace.out

--- smot/api_tests/torch_api/constructors/linspace_test.py
+++ smot/api_tests/torch_api/constructors/linspace_test.py
@@ -1,8 +1,9 @@
 import unittest
 
+import hamcrest
 import torch
 
-from smot.testlib import torch_eggs
+from smot.testlib import eggs, torch_eggs
 
 
 class LinspaceTest(unittest.TestCase):
@@ -18,6 +19,43 @@ class LinspaceTest(unittest.TestCase):
         torch_eggs.assert_tensor(
             torch.linspace(10, 3, steps=5),
             [10.0, 8.25, 6.5, 4.75, 3.0],
+        )
+
+    def test_linspace_out(self):
+        t = torch.ones(5)
+        original_data = t.data_ptr()
+
+        # same size, same data ptr.
+        torch_eggs.assert_tensor(
+            torch.linspace(3, 10, steps=5, out=t),
+            [3.0, 4.75, 6.5, 8.25, 10.0],
+        )
+
+        eggs.assert_match(
+            t.data_ptr(),
+            original_data,
+        )
+
+        # smaller size, same data ptr.
+        torch_eggs.assert_tensor(
+            torch.linspace(3, 10, steps=3, out=t),
+            [3.0, 6.5, 10.0],
+        )
+
+        eggs.assert_match(
+            t.data_ptr(),
+            original_data,
+        )
+
+        # larger size, NEW data ptr.
+        torch_eggs.assert_tensor(
+            torch.linspace(1, 7, steps=7, out=t),
+            [1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0],
+        )
+
+        eggs.assert_match(
+            t.data_ptr(),
+            hamcrest.not_(original_data),
         )
 
     def test_linspace_100(self):
--- smot/api_tests/torch_api/constructors/logspace_test.py
+++ smot/api_tests/torch_api/constructors/logspace_test.py
@@ -1,8 +1,9 @@
 import unittest
 
+import hamcrest
 import torch
 
-from smot.testlib import torch_eggs
+from smot.testlib import eggs, torch_eggs
 
 
 class LogspaceTest(unittest.TestCase):
@@ -18,6 +19,43 @@ class LogspaceTest(unittest.TestCase):
         torch_eggs.assert_tensor(
             torch.logspace(10, -10, steps=5),
             [1.0e10, 1.0e05, 1.0e00, 1.0e-05, 1.0e-10],
+        )
+
+    def test_logspace_out(self):
+        t = torch.ones(5)
+        original_data = t.data_ptr()
+
+        # same size, same data ptr.
+        torch_eggs.assert_tensor(
+            torch.logspace(-10, 10, steps=5, out=t),
+            [1.0e-10, 1.0e-05, 1.0e00, 1.0e05, 1.0e10],
+        )
+
+        eggs.assert_match(
+            t.data_ptr(),
+            original_data,
+        )
+
+        # smaller size, same data ptr.
+        torch_eggs.assert_tensor(
+            torch.logspace(-10, 10, steps=3, out=t),
+            [1.0e-10, 1.0e00, 1.0e10],
+        )
+
+        eggs.assert_match(
+            t.data_ptr(),
+            original_data,
+        )
+
+        # larger size, NEW data ptr.
+        torch_eggs.assert_tensor(
+            torch.logspace(-12, 12, steps=9, out=t),
+            [1.0e-12, 1.0e-09, 1.0e-06, 1.0e-03, 1.0e0, 1.0e03, 1.0e06, 1.0e09, 1.0e12],
+        )
+
+        eggs.assert_match(
+            t.data_ptr(),
+            hamcrest.not_(original_data),
         )
 
     def test_logspace_100(self):
