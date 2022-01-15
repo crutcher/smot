ecfe031340fa761dd4e81c59f36e7b3186404a81
Crutcher Dunnavant <crutcher@gmail.com>
2022-01-15 03:09:26 -0800

    heaviside; autoflake

---
+++ smot/api_tests/torch_api/constructors/heaviside_test.py
@@ -0,0 +1,39 @@
+import unittest
+
+import torch
+
+from smot.testlib import eggs, torch_eggs
+
+
+class HeavisideTest(unittest.TestCase):
+    # https://pytorch.org/docs/stable/generated/torch.heaviside.html
+
+    def test_heaviside_scalar_value(self):
+        input = torch.tensor([-1.5, 0, 2.0])
+        values = torch.tensor(0.5)
+        torch_eggs.assert_tensor(
+            torch.heaviside(input, values),
+            torch.tensor([0.0, 0.5, 1.0]),
+        )
+
+    def test_heaviside_single_value(self):
+        input = torch.tensor([-1.5, 0, 2.0])
+        values = torch.tensor([0.5])
+        torch_eggs.assert_tensor(
+            torch.heaviside(input, values),
+            torch.tensor([0.0, 0.5, 1.0]),
+        )
+
+    def test_heaviside_multi_value(self):
+        input = torch.tensor([-1.5, 0, 0])
+
+        eggs.assert_raises(
+            lambda: torch.heaviside(input, torch.tensor([0.5, 2])),
+            RuntimeError,
+            r"must match the size of tensor b \(2\) at non-singleton dimension 0",
+        )
+
+        torch_eggs.assert_tensor(
+            torch.heaviside(input, torch.tensor([5.0, 6.0, 7.0])),
+            torch.tensor([0.0, 6.0, 7.0]),
+        )
