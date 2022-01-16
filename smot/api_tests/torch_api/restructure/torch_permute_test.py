c550b55a6299fbac3ff2ef21b0d1fef866ad4aff
Crutcher Dunnavant <crutcher@gmail.com>
2022-01-16 15:52:26 -0800

    torch.permute

---
+++ smot/api_tests/torch_api/restructure/torch_permute_test.py
@@ -0,0 +1,39 @@
+import unittest
+
+import torch
+
+from smot.testlib import torch_eggs
+
+
+class PermuteTest(unittest.TestCase):
+    def test_basic(self):
+        source = torch.tensor(
+            [
+                [
+                    [1, 2],
+                    [3, 4],
+                ],
+                [
+                    [5, 6],
+                    [7, 8],
+                ],
+            ]
+        )
+
+        view = torch.permute(source, (2, 0, 1))
+
+        torch_eggs.assert_view(view, source)
+
+        torch_eggs.assert_tensor(
+            view,
+            [
+                [
+                    [1, 3],
+                    [5, 7],
+                ],
+                [
+                    [2, 4],
+                    [6, 8],
+                ],
+            ],
+        )
