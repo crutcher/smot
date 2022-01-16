81c5c0871f8d0d0c4e7fbc300fdf5ab241a7e03d
Crutcher Dunnavant <crutcher@gmail.com>
2022-01-16 14:06:26 -0800

    np.masked_select

---
+++ smot/api_tests/torch_api/restructure/torch_masked_select_test.py
@@ -0,0 +1,33 @@
+import unittest
+
+import torch
+
+from smot.testlib import torch_eggs
+
+
+class MaskedSelectTest(unittest.TestCase):
+    def test_select(self):
+        source = torch.arange(9).reshape(3, 3)
+
+        torch_eggs.assert_tensor(
+            source,
+            [
+                [0, 1, 2],
+                [3, 4, 5],
+                [6, 7, 8],
+            ],
+        )
+
+        torch_eggs.assert_tensor(
+            torch.masked_select(
+                source,
+                torch.tensor(
+                    [
+                        [True, False, False],
+                        [True, False, True],
+                        [False, True, True],
+                    ]
+                ),
+            ),
+            [0, 3, 5, 7, 8],
+        )
