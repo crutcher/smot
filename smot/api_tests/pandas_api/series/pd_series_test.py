db436da8f5f0f0043a8265de1f977cb75007375a
Crutcher Dunnavant <crutcher@gmail.com>
2022-01-16 12:29:05 -0800

    basic pandas

---
+++ smot/api_tests/pandas_api/series/pd_series_test.py
@@ -0,0 +1,25 @@
+import unittest
+
+import hamcrest
+import numpy as np
+import pandas as pd
+
+from smot.testlib import eggs, np_eggs
+
+
+class SeriesTest(unittest.TestCase):
+    def test_simple(self):
+        s = pd.Series(["a", "b", "c"])
+
+        eggs.assert_match(
+            s.axes,
+            hamcrest.contains_exactly(
+                hamcrest.contains_exactly(
+                    *pd.RangeIndex(0, 3),
+                )
+            ),
+        )
+        np_eggs.assert_ndarray(
+            s.values,
+            np.array(["a", "b", "c"], dtype=object),
+        )
