196e748b945693d8b61939e5d90fa28169412cad
Crutcher Dunnavant <crutcher@gmail.com>
2022-01-16 11:35:31 -0800

    refactor ones test

--- smot/api_tests/torch_api/constructors/torch_ones_test.py
+++ smot/api_tests/torch_api/constructors/torch_ones_test.py
@@ -1,7 +1,6 @@
 import unittest
 
 import hamcrest
-import pytest
 import torch
 
 from smot.testlib import eggs, torch_eggs
@@ -63,58 +62,4 @@ class OnesTest(unittest.TestCase):
         eggs.assert_match(
             out.data_ptr(),
             hamcrest.not_(original_data),
-        )
-
-
-class OnesLikeTest(unittest.TestCase):
-    # https://pytorch.org/docs/stable/generated/torch.ones_like.html
-
-    def dense(self, device):
-        # Dense Tensors
-        for dtype in [torch.int8, torch.float32]:
-            source = torch.tensor(
-                [1, 2],
-                dtype=dtype,
-                device=device,
-            )
-
-            torch_eggs.assert_tensor(
-                torch.ones_like(source),
-                torch.ones(
-                    *source.size(),
-                    dtype=source.dtype,
-                    device=source.device,
-                    layout=source.layout,
-                ),
-            )
-
-    def test_cpu_dense(self):
-        self.dense("cpu")
-
-    @pytest.mark.slow
-    def test_cuda_dense(self):
-        if torch.cuda.is_available():
-            self.dense("cuda")
-
-    def test_sparse(self):
-        coo = torch.tensor([[0, 0], [2, 2]])
-        vals = torch.tensor([3, 4])
-        source = torch.sparse_coo_tensor(
-            indices=coo,
-            values=vals,
-        )
-
-        # torch.ones_like() refuses to play nice with sparse coo tensors.
-        #
-        # Arguably, this makes sense. There would only be two valid answers:
-        #  1. a sparse-ones tensor, filled in at places the source had presence.
-        #  2. an entirely filled in tensor (which ... why sparse at that point)?
-        #
-        # However, rather than just throwing an AssertionError telling you
-        # that you can't do this, it throws a NotImplementedError complaining
-        # about the tensor backend provider not supporting the operation.
-        eggs.assert_raises(
-            lambda: torch.ones_like(source),
-            NotImplementedError,
-            "with arguments from the 'SparseCPU' backend",
         )
