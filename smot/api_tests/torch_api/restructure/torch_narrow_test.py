1d19c93bd89d7f9b02c6915821ce92774180ed62
Crutcher Dunnavant <crutcher@gmail.com>
2022-01-16 14:57:19 -0800

    np.narrow

---
+++ smot/api_tests/torch_api/restructure/torch_narrow_test.py
@@ -0,0 +1,43 @@
+import unittest
+
+import torch
+
+from smot.testlib import torch_eggs
+
+
+class NarrowTest(unittest.TestCase):
+    # https://pytorch.org/docs/stable/generated/torch.narrow.html
+
+    def test_narrow(self):
+        source = torch.tensor(
+            [
+                [1, 2, 3],
+                [4, 5, 6],
+                [7, 8, 9],
+            ]
+        )
+
+        view = torch.narrow(source, 0, 1, 2)
+
+        torch_eggs.assert_view(view, source)
+
+        torch_eggs.assert_tensor(
+            view,
+            [
+                [4, 5, 6],
+                [7, 8, 9],
+            ],
+        )
+
+        view = torch.narrow(source, 1, 0, 1)
+
+        torch_eggs.assert_view(view, source)
+
+        torch_eggs.assert_tensor(
+            view,
+            [
+                [1],
+                [4],
+                [7],
+            ],
+        )
