0024aa972fdcbe839d356aa272736a5918ef9a63
Crutcher Dunnavant <crutcher@gmail.com>
2022-01-18 08:16:15 -0800

    generator

--- smot/api_tests/torch_api/random/torch_Generator_test.py
+++ smot/api_tests/torch_api/random/torch_Generator_test.py
@@ -1,3 +1,4 @@
+import hamcrest
 import pytest
 import torch
 
@@ -31,9 +32,25 @@ class GeneratorTest(TorchApiTestCase):
                 torch.device("cuda"),
             )
 
-    def test_manual_seed(self) -> None:
+    def test_initial_seed(self) -> None:
         g = torch.Generator()
         seed = g.initial_seed()
+
+        reference = torch.rand([2, 3], generator=g)
+
+        # Replay with same seed.
+        g.manual_seed(seed)
+
+        torch_eggs.assert_tensor(
+            torch.rand([2, 3], generator=g),
+            reference,
+        )
+
+    def test_manual_seed(self) -> None:
+        seed = 123324523
+
+        g = torch.Generator()
+        g.manual_seed(seed)
 
         reference = torch.rand([2, 3], generator=g)
 
@@ -66,13 +83,42 @@ class GlobalGeneratorTest(TorchApiTestCase):
         torch.initial_seed: "https://pytorch.org/docs/stable/generated/torch.initial_seed.html",
         torch.get_rng_state: "https://pytorch.org/docs/stable/generated/torch.get_rng_state.html",
         torch.set_rng_state: "https://pytorch.org/docs/stable/generated/torch.set_rng_state.html",
+        torch.default_generator: "https://pytorch.org/docs/stable/torch.html#torch.torch.default_generator",
     }
 
     API_DOC = "https://pytorch.org/docs/stable/generated/torch.seed.html"
     TARGET = torch.seed
 
+    def test_initial_seed(self) -> None:
+        orig_seed = torch.initial_seed()
+
+        # NOTE: you probably shouldn't use this.
+        # this is the initial seed for the default generator,
+        eggs.assert_match(
+            orig_seed,
+            torch.default_generator.initial_seed(),
+        )
+
+        state = torch.get_rng_state()
+
+        # but it is not equivalent to the current seed:
+        torch.rand([2, 3])
+
+        # sampling from the generator changes the state:
+        eggs.assert_match(
+            torch.get_rng_state(),
+            hamcrest.not_(torch_eggs.expect_tensor(state)),
+        )
+
+        # but does not change the seed.
+        eggs.assert_match(
+            orig_seed,
+            torch.default_generator.initial_seed(),
+        )
+
     def test_manual_seed(self) -> None:
-        seed = torch.initial_seed()
+        seed = 12312323
+        torch.manual_seed(seed)
 
         reference = torch.rand([2, 3])
 
@@ -81,6 +127,17 @@ class GlobalGeneratorTest(TorchApiTestCase):
 
         torch_eggs.assert_tensor(
             torch.rand([2, 3]),
+            reference,
+        )
+
+        # Pull the default generator
+        g = torch.default_generator
+
+        # Replay with same seed, using the generator ref.
+        g.manual_seed(seed)
+
+        torch_eggs.assert_tensor(
+            torch.rand([2, 3], generator=g),
             reference,
         )
 
